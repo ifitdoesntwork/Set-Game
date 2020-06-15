449e908299a7ee5b38a7cc5a03fbb295594bb196
Denis Avdeev <davdeev@me.com>
2020-06-15 10:22:02 +0500

    Initial Commit

---
+++ Set Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Set Game
+//
+//  Created by Denis Avdeev on 15.06.2020.
+//  Copyright © 2020 Denis Avdeev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
